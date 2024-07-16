6f473b8114f95e849b274a04b210f77c6248f0df
Muralidharan Deenathayalan <muralidharand@users.noreply.github.com>
2024-07-16 21:16:11 +0530

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Muralidharan Deenathayalan on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
